06c20b7d4f7e7b1d9d6e886945ceb631c92ffafe
RemainingToast <remainingtoast@gmail.com>
2021-03-19 22:33:08 +1000

    Small code cleanup

--- src/main/java/com/github/fabricutilitymods/friendapi/FriendData.java
+++ src/main/java/com/github/fabricutilitymods/friendapi/FriendData.java
@@ -2,12 +2,10 @@ package com.github.fabricutilitymods.friendapi;
 
 import com.mojang.authlib.GameProfile;
 
-import java.util.UUID;
-
 public interface FriendData {
 
-    FriendType getType();
-
     GameProfile getProfile();
+
+    FriendType getType();
 
 }
